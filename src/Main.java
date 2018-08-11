7e2e312602d49181765d7b3d12f0f442d0575b9e
itcast <itheima@cast.com>
2018-08-11 15:36:12 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("尼玛的以乐闻草泥马第一");
+        String sdf = new String("sdf");
+        System.out.println(sdf);
+    }
+}
